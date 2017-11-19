a18ea9c5ee53c50d2f70d6b663309533296ec498
Maxim Suprunenko <suprmaks@gmail.com>
2017-11-19 20:38:26 +0200

    Add test project

---
+++ LittleSocialNetwork.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace LittleSocialNetwork.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void STest()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
